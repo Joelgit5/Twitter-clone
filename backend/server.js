289b6d63b21270029a4f605d2ad93966946fb32b
Muhammad Usman <119290487+Usman-1117@users.noreply.github.com>
2024-04-28 21:38:49 +0500

    Project Setup

---
+++ backend/server.js
@@ -0,0 +1,15 @@
+import express from "express";
+import dotenv from "dotenv";
+import dbConnect from "./db/ConnectMongoDB.js";
+
+const app = express();
+dotenv.config();
+
+//* DataBase Connection
+dbConnect();
+
+//* PORT Assign
+const PORT = process.env.PORT || 9000;
+
+//* Running App
+app.listen(PORT, () => console.log(`Server Runing on ${PORT}`));
