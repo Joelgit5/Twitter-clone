ebd82bd3e7f11eb9dd24f6601f2cab52d66aa28e
Muhammad Usman <119290487+Usman-1117@users.noreply.github.com>
2024-05-11 17:02:10 +0500

    Auth Controllers Added

---
+++ backend/models/user.model.js
@@ -0,0 +1,64 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    fullName: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      minLength: 8,
+    },
+
+    followers: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        default: [],
+      },
+    ],
+
+    following: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        default: [],
+      },
+    ],
+
+    profileImg: {
+      type: String,
+      default: "",
+    },
+    coverImg: {
+      type: String,
+      default: "",
+    },
+    bio: {
+      type: String,
+      default: "",
+    },
+    link: {
+      type: String,
+      default: "",
+    },
+  },
+  { timestamps: true }
+);
+
+//Export the model
+const User = mongoose.model("User", userSchema);
+
+export default User;
--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,21 @@
 import express from "express";
 import dotenv from "dotenv";
 import dbConnect from "./db/ConnectMongoDB.js";
+import authRoutes from "./routes/auth.route.js";
+import cookieParser from "cookie-parser";
 
 const app = express();
 dotenv.config();
 
 //* DataBase Connections
 dbConnect();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+//* Routes
+app.use("/api/auth", authRoutes);
 
 //* PORT Assign
 const PORT = process.env.PORT || 9000;
